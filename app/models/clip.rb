4834117b0bdb032dc9b1a32fb504ad4f0ed73ef6
jcap49 <jcapecelatro@gmail.com>
2013-04-15 16:53:42 -0700

    added validations to clip model

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -1,4 +1,13 @@
 class Clip < ActiveRecord::Base
+
+  # Attrs
   attr_accessible :title, :body, :photo
   has_attached_file :photo
+
+  # Validations
+  validates :title, presence: true, length: {maximum: 250}
+  validates :body, presence: true, length: {maximum: 1000}
+  validates :photo, presence: true
+
+
 end
