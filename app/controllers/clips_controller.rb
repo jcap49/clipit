50cd2daf68c52c0b3cdb283961e486a7c9c9f2b7
jcap49 <jcapecelatro@gmail.com>
2013-04-23 06:46:07 -0700

    fixed static text bug --> dynamic

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -79,6 +79,10 @@ class ClipsController < ApplicationController
 
   def example
     @clip = Clip.limit(1).offset(rand(Clip.count)).first
+    @video = yt_client.videos_by(:query => @clip.song, :max_results => "1", :format => "5", :autoplay => "1")
+
+    @html_for_video = @video.videos
+    @e_video = @html_for_video.collect {|video| video.embed_html5(:url_params => {:autoplay => "1"})}
   end
 
   def video_results
