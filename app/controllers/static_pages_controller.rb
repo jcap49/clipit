c83007c39d55dbd847eae9392390081fbbe2bf6b
jcap49 <jcapecelatro@gmail.com>
2013-04-29 19:57:58 -0700

    some big styling changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
+    @clip = Clip.limit(1).offset(rand(Clip.count)).first
+    @video = yt_client.videos_by(:query => @clip.song, :max_results => "1", :format => "5", :autoplay => "1")
+
+    @html_for_video = @video.videos
+    @e_video = @html_for_video.collect {|video| video.embed_html5(:url_params => {:autoplay => "1"})}
   end
 
   def why
