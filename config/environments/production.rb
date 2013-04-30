01f7c8703b1d8185bccb46e04349b1a02d5a82b6
jcap49 <jcapecelatro@gmail.com>
2013-04-29 17:55:17 -0700

    more mailer stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ ClipIt::Application.configure do
 }
   # for mailer
   config.action_mailer.default_url_options = { :host => 'clipitt.herokuapp.com' }
-  config.action_mailer.delivery_method :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
