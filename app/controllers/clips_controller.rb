d27bf0f1d5ba0f38c47ffd01da3574f8863c11e7
jcap49 <jcapecelatro@gmail.com>
2013-05-03 08:58:04 -0700

    more styling stuff

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -8,7 +8,7 @@ class ClipsController < ApplicationController
 
   def show
     @clip = Clip.find(params[:id])
-    @video = yt_client.videos_by(:query => @clip.song, :max_results => "1", :format => "5", :autoplay => "1")
+    @video = yt_client.videos_by(:query => @clip.song, :max_results => "1", :format => "5", :autoplay => "1", :controls => "2")
 
     @html_for_video = @video.videos
     @e_video = @html_for_video.collect {|video| video.embed_html5(:url_params => {:autoplay => "1"})}
